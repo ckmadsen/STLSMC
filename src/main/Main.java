edb2cc5b187aadb719cf129e6bb06802c6d6f9fe
Curtis Madsen <ckmadsen@bu.edu>
2017-05-01 03:35:35 -0400

    Updated STL code including robustness checker and added support for confidence interval specifications.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,22 @@
 package main;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import checker.STLChecker;
+import parser.STLAbstractSyntaxTreeExtractor;
+import parser.STLLexer;
+import parser.STLParser;
+import stl.TreeNode;
+
 import data.Trace;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.ParserRuleContext;
+
+import org.apache.commons.math3.distribution.NormalDistribution;
 
 public class Main {
 
@@ -16,35 +26,44 @@ public class Main {
 				if (args[0].equals("-h") || args[0].equals("--h") || args[0].equals("-help")
 						|| args[0].equals("--help")) {
 					System.out
-							.println("Usage:  java -jar STLSMC-<version>.jar <directory_of_csv_traces> <property_to_check>");
+							.println("Usage:  java -jar SMC4STL-<version>.jar <confidence_interval> <directory_of_csv_traces> <property_to_check>");
 					return;
 				}
-				String property = args[1];
-				for (int i = 2; i < args.length; i++) {
-					property += args[i];
-				}
-				File dir = new File(args[0]);
-				if (!dir.isDirectory()) {
-					System.out.println("First argument must be a directory of CSV trace files.");
-				}
-				else {
-					List<String> traceFiles = new ArrayList<String>();
-					for (File file : dir.listFiles()) {
-						if (file.getName().endsWith(".csv") || file.getName().endsWith(".CSV")) {
-							traceFiles.add(file.getAbsolutePath());
-						}
+				if (args.length > 2) {
+					String property = args[2];
+					for (int i = 3; i < args.length; i++) {
+						property += args[i];
 					}
-					double satisfyingPercent = computeSatisfyingPercent(traceFiles, property);
-					System.out.println((satisfyingPercent * 100) + "% of traces satisfy the property.");
-					System.out.println("The true satisfying percentage is within "
-							+ (computeError(satisfyingPercent, traceFiles.size()) * 100)
-							+ "% of this value with 95% confidence.");
+					double confidenceInterval = Double.parseDouble(args[0]);
+					File dir = new File(args[1]);
+					if (!dir.isDirectory()) {
+						System.out.println("Second argument must be a directory of CSV trace files.");
+					}
+					else {
+						List<String> traceFiles = new ArrayList<String>();
+						for (File file : dir.listFiles()) {
+							if (file.getName().endsWith(".csv") || file.getName().endsWith(".CSV")) {
+								traceFiles.add(file.getAbsolutePath());
+							}
+						}
+						double satisfyingPercent = computeSatisfyingPercent(traceFiles, property);
+						double error = computeError(satisfyingPercent, traceFiles.size(), confidenceInterval);
+						System.out.printf("%.2f", (satisfyingPercent * 100));
+						System.out.println("% of traces satisfy the property.");
+						System.out.print("The true satisfying percentage is between ");
+						System.out.printf("%.2f", Math.max(((satisfyingPercent - error) * 100), 0.0));
+						System.out.print("% and ");
+						System.out.printf("%.2f", Math.min(((satisfyingPercent + error) * 100), 100.0));
+						System.out.print("% with ");
+						System.out.printf("%.2f", (confidenceInterval * 100));
+						System.out.println("% confidence.");
+					}
 				}
 			}
 			else {
 				System.out.println("Incorrect number of arguments.");
 				System.out
-						.println("Usage:  java -jar STLSMC-<version>.jar <directory_of_csv_traces> <property_to_check>");
+						.println("Usage:  java -jar SMC4STL-<version>.jar <confidence_interval> <directory_of_csv_traces> <property_to_check>");
 			}
 		}
 		catch (IOException e) {
@@ -53,13 +72,13 @@ public class Main {
 		}
 	}
 
-	public static double getRobustness(String trace, String property) throws IOException {
-		return getRobustness(Trace.parseCSV(trace), property);
-	}
-
-	public static double getRobustness(Trace trace, String property) {
-		STLChecker check = new STLChecker(trace);
-		return check.robustness(property);
+	public static double getRobustness(String trace, String property) throws FileNotFoundException {
+		STLLexer lexer = new STLLexer(new ANTLRInputStream(property));
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		STLParser parser = new STLParser(tokens);
+		ParserRuleContext t = parser.property();
+		TreeNode ast = new STLAbstractSyntaxTreeExtractor().visit(t);
+		return ast.robustness(Trace.parseCSV(trace), 0);
 	}
 
 	public static double computeSatisfyingPercent(List<String> traces, String property) throws IOException {
@@ -72,8 +91,10 @@ public class Main {
 		return ((double) sat) / ((double) traces.size());
 	}
 
-	public static double computeError(double satisfyingPercent, int numTraces) {
-		return 1.96 * Math.sqrt((1 - satisfyingPercent) / (satisfyingPercent * numTraces));
+	public static double computeError(double satisfyingPercent, int numTraces, double confidenceInterval) {
+		NormalDistribution distribution = new NormalDistribution(0, 1);
+		double confidenceValue = distribution.inverseCumulativeProbability(confidenceInterval + ((1 - confidenceInterval) / 2));
+		return confidenceValue * Math.sqrt((1 - satisfyingPercent) / (satisfyingPercent * numTraces));
 	}
 
 }
